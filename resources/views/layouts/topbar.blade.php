b56615aefce9df6d1745340e01326758b0a38041
Ridho Hakiki <ridhohakiki44@gmail.com>
2024-08-06 18:06:55 +0700

    Memperbarui topbar

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -297,7 +297,7 @@
         <li class="nav-item navbar-dropdown dropdown-user dropdown">
             <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
             <div class="avatar avatar-online">
-                <img src="{{ asset('backend/assets/img/avatars/1.png') }}" alt class="h-auto rounded-circle" />
+                <img src="{{ auth()->user()->avatar ? asset('storage/' . auth()->user()->avatar) : asset('storage/avatars/default-avatar.png') }}" alt class="h-auto rounded-circle" />
             </div>
             </a>
             <ul class="dropdown-menu dropdown-menu-end">
@@ -306,7 +306,7 @@
                 <div class="d-flex">
                     <div class="flex-shrink-0 me-3">
                     <div class="avatar avatar-online">
-                        <img src="{{ asset('backend/assets/img/avatars/1.png') }}" alt class="h-auto rounded-circle" />
+                        <img src="{{ auth()->user()->avatar ? asset('storage/' . auth()->user()->avatar) : asset('storage/avatars/default-avatar.png') }}" alt class="h-auto rounded-circle" />
                     </div>
                     </div>
                     <div class="flex-grow-1">
