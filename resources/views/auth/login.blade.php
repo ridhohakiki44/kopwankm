948665ffa687b742a851d13fc6d18a6670f5e0a2
Ridho Hakiki <ridhohakiki44@gmail.com>
2024-08-13 20:36:19 +0700

    Menambahkan pesan error di login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,12 @@
 <!doctype html>
 
-<html
-  lang="en"
-  class="light-style layout-wide customizer-hide"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="{{ asset('backend/assets/') }}"
-  data-template="vertical-menu-template">
-  <head>
+<html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default"
+    data-assets-path="{{ asset('backend/assets/') }}" data-template="vertical-menu-template">
+
+<head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Login | Kopwan-KM</title>
 
@@ -24,8 +19,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
-      rel="stylesheet" />
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+        rel="stylesheet" />
 
     <!-- Icons -->
     <link rel="stylesheet" href="{{ asset('backend/assets/vendor/fonts/fontawesome.css') }}" />
@@ -33,8 +28,10 @@
     <link rel="stylesheet" href="{{ asset('backend/assets/vendor/fonts/flag-icons.css') }}" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/rtl/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/rtl/theme-default.css') }}" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/rtl/core.css') }}"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/rtl/theme-default.css') }}"
+        class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('backend/assets/css/demo.css') }}" />
 
     <!-- Vendors CSS -->
@@ -55,98 +52,91 @@
     <script src="{{ asset('backend/assets/vendor/js/template-customizer.js') }}"></script>
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="{{ asset('backend/assets/js/config.js') }}"></script>
-  </head>
+</head>
 
-  <body>
+<body>
     <!-- Content -->
 
     <div class="container-xxl">
-      <div class="authentication-wrapper authentication-basic container-p-y">
-        <div class="authentication-inner py-4">
-          <!-- Login -->
-          <div class="card">
-            <div class="card-body">
-              <!-- Logo -->
-              <div class="app-brand justify-content-center mb-4 mt-2">
-                <div class="app-brand-link gap-2">
-                  <span class="app-brand-logo demo">
-                    <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
-                        fill="#7367F0" />
-                      <path
-                        opacity="0.06"
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
-                        fill="#161616" />
-                      <path
-                        opacity="0.06"
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
-                        fill="#161616" />
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
-                        fill="#7367F0" />
-                    </svg>
-                  </span>
-                  <span class="app-brand-text demo text-body fw-bold ms-1">Kopwan-KM</span>
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner py-4">
+                <!-- Login -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center mb-4 mt-2">
+                            <div class="app-brand-link gap-2">
+                                <span class="app-brand-logo demo">
+                                    <svg width="32" height="22" viewBox="0 0 32 22" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
+                                            fill="#7367F0" />
+                                        <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
+                                            fill="#161616" />
+                                        <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
+                                            fill="#161616" />
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
+                                            fill="#7367F0" />
+                                    </svg>
+                                </span>
+                                <span class="app-brand-text demo text-body fw-bold ms-1">Kopwan-KM</span>
+                            </div>
+                        </div>
+                        <!-- /Logo -->
+
+                        <form method="POST" action="{{ route('login') }}" id="formAuthentication" class="mb-3">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="text" class="form-control" id="email" name="email"
+                                    placeholder="Masukan email" autofocus />
+
+                                @if ($errors->has('email'))
+                                    <div class="mt-1 text-danger">
+                                        <small>{{ $errors->first('email') }}</small>
+                                    </div>
+                                @endif
+                            </div>
+
+                            <div class="mb-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <label class="form-label" for="password">Password</label>
+                                    <a href="{{ route('password.request') }}">
+                                        <small>Lupa Password?</small>
+                                    </a>
+                                </div>
+
+                                <input type="password" id="password" class="form-control" name="password"
+                                    placeholder="Masukan password" aria-describedby="password" />
+
+                                @if ($errors->has('password'))
+                                    <div class="mt-1 text-danger">
+                                        <small>{{ $errors->first('password') }}</small>
+                                    </div>
+                                @endif
+                            </div>
+
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" type="submit">Login</button>
+                            </div>
+                        </form>
+
+                        <p class="text-center">
+                            <span>Belum punya akun?</span>
+                            <a href="{{ route('register') }}">
+                                <span>Buat akun</span>
+                            </a>
+                        </p>
+
+                    </div>
                 </div>
-              </div>
-              <!-- /Logo -->
-
-              <form method="POST" action="{{ route('login') }}" id="formAuthentication" class="mb-3">
-                @csrf
-                <div class="mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="email"
-                    name="email"
-                    placeholder="Masukan email"
-                    autofocus />
-                </div>
-
-                <div class="mb-3 form-password-toggle">
-                  <div class="d-flex justify-content-between">
-                    <label class="form-label" for="password">Password</label>
-                    <a href="{{ route('password.request') }}">
-                      <small>Lupa Password?</small>
-                    </a>
-                  </div>
-                  
-                  <input
-                    type="password"
-                    id="password"
-                    class="form-control"
-                    name="password"
-                    placeholder="Masukan password"
-                    aria-describedby="password" />
-                </div>
-
-                <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Login</button>
-                </div>
-              </form>
-
-              <p class="text-center">
-                <span>Belum punya akun?</span>
-                <a href="{{ route('register') }}">
-                  <span>Buat akun</span>
-                </a>
-              </p>
-
+                <!-- /Register -->
             </div>
-          </div>
-          <!-- /Register -->
         </div>
-      </div>
     </div>
 
     <!-- / Content -->
@@ -173,9 +163,10 @@
 
     <!-- Main JS -->
     <script src="{{ asset('backend/assets/js/main.js') }}"></script>
-    
+
 
     <!-- Page JS -->
     <script src="{{ asset('backend/assets/js/pages-auth.js') }}"></script>
-  </body>
+</body>
+
 </html>
