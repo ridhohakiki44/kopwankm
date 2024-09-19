cfcc497b856045e972aa118c958fc8b5c8145d1b
Ridho Hakiki <ridhohakiki44@gmail.com>
2024-09-19 20:57:56 +0700

    Mengubah logo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,7 +18,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('backend/assets/img/favicon/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('backend/assets/img/favicon/logo-koperasi.ico') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
